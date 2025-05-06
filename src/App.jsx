3a41d619c794042abbb70aaa7294b0f21dcc06d0
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-05-06 08:12:26 -0400

    Inital Files

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import WhatWeOffer from './components/WhatWeOffer'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Subscribe from './components/Subscribe'
+import Footer from './components/Footer'
+
+
+function App() {
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <WhatWeOffer></WhatWeOffer>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Subscribe></Subscribe>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
